1f62f5e653349433dc88beafe0b49832ba9b530d
kato1208 <taichi-8128@outlook.jp>
2020-08-24 23:40:14 +0900

    form todo list

--- todo/src/App.js
+++ todo/src/App.js
@@ -2,26 +2,57 @@ import React, { Component } from 'react';
 import './App.css';
 import ToDoListItem from "./ToDoListItem"
 
-function App() {
-  return (
-    <div className="App">
-      <form className="App-form">
+class App extends Component {
+
+  state = {
+    todoList: []
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form 
+        className="App-form"
+        onSubmit={e => {
+          e.preventDefault();
+
+          const titleElement = e.target.elements['title'];
+          const descriptionElement = e.target.elements['description'];
+
+          this.setState(
+            {
+              todoList: this.state.todoList.concat({
+                title: titleElement.value,
+                description: descriptionElement.value
+              })
+            },
+            () => {
+              titleElement.value = "";
+              descriptionElement.value = "";
+            }
+          )
+        }}
+        >
+          <div>
+            <input id="title" placeholder="title" />
+            <textarea id="description" placeholder="description" />
+          </div>
+          <div>
+            <button type="submit">登録</button>
+          </div>
+        </form>
         <div>
-          <input id="title" placeholder="title" />
-          <textarea id="description" placeholder="description" />
+          {this.state.todoList.map(todo => (
+            <ToDoListItem
+              key={todo.title}
+              title={todo.title}
+              description={todo.description}
+            />
+          ))}
         </div>
-        <div>
-          <button type="submit">登録</button>
-        </div>
-      </form>
-      <div>
-        <ToDoListItem 
-        title="ホームページ作成" 
-        description="サークルホームページを今週中に作成する" 
-        />
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
