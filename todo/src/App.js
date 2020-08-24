d9e61cf0803c7087954e5a50216f12dee0b01efd
kato1208 <taichi-8128@outlook.jp>
2020-08-24 23:16:10 +0900

    create button

--- todo/src/App.js
+++ todo/src/App.js
@@ -5,6 +5,15 @@ import ToDoListItem from "./ToDoListItem"
 function App() {
   return (
     <div className="App">
+      <form className="App-form">
+        <div>
+          <input id="title" placeholder="title" />
+          <textarea id="description" placeholder="description" />
+        </div>
+        <div>
+          <button type="submit">登録</button>
+        </div>
+      </form>
       <div>
         <ToDoListItem 
         title="ホームページ作成" 
